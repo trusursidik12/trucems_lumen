3f63bd640023d0ac492d1a0c702aff1cdc87ff76
Irwan Antonio <irwanantonio2708@gmail.com>
2022-05-25 17:55:35 +0700

    process cal route

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,17 @@ use App\Http\Controllers\ExampleController;
 */
 
 $router->get('/', 'DashboardController@index');
-$router->group(['prefix' => 'calibration'], function () use ($router) {
+$router->group(['prefix' => 'calibration'], function() use ($router){
     $router->get('/manual', 'CalibrationController@manual');
     $router->get('/auto', 'CalibrationController@auto');
     $router->get('/logs', 'CalibrationController@logs');
+    $router->get('/{mode}/{type}/process', 'CalibrationController@processCal');
+
 });
 /**
  * API
  */
-$router->group(['prefix' => 'api'], function () use ($router) {
+$router->group(['prefix' => 'api'], function() use ($router){
     /**
      * Set Calibration
      */
@@ -51,6 +53,8 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     /**
      * Configurations
      */
-    $router->get('/configurations', 'API\ConfigurationController@index');
-    $router->patch('/configurations', 'API\ConfigurationController@update');
+    $router->get('/configurations','API\ConfigurationController@index');
+    $router->patch('/configurations','API\ConfigurationController@update');
+
+    
 });
