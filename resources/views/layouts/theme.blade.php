7bc3f4d159e561b3c36460a1ce282e1d322877a6
Irwan Antonio <irwanantonio2708@gmail.com>
2022-06-02 10:04:24 +0700

    add label runtime & add favicon

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ url('favicon.png') }}" type="image/x-icon">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
     @yield('css')
