578d98667668dcd67ca52205b38df1c4ee9735c6
dulurprojects2019 <trusursidik12@gmail.com>
2022-07-19 16:01:03 +0700

    a

--- run/witec_reader_prod.py
+++ run/witec_reader_prod.py
@@ -87,12 +87,13 @@ try:
                     if(json_get_configuration["data"]["is_calibration"] == 1):
                         round_value = round(float(data_value), 2)
                     else:
-                        if(data_value < 0):
-                            data_value = 0
-                        else:
-                            data_value = data_value
-                        round_value = round(
-                            float((data_value if data_value >= 0 else 0)), 2)
+                        round_value = round(float(data_value), 2)
+                        # if(data_value < 0):
+                        #     data_value = 0
+                        # else:
+                        #     data_value = data_value
+                        # round_value = round(
+                        #     float((data_value if data_value >= 0 else 0)), 2)
                 else:
                     # value set when the sensor disconnected!
                     round_value = -2.222
