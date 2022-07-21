608e7a647e91190abc4d959312299feb865a1d2d
dulurprojects2019 <trusursidik12@gmail.com>
2022-07-21 18:04:27 +0700

    button cga

--- resources/views/calibration/manual.blade.php
+++ resources/views/calibration/manual.blade.php
@@ -2,16 +2,20 @@
 @section('title', 'Manual Calibration')
 @section('content')
     <div class="px-6 py-3 bg-gray-200 rounded">
-       
+
         <div id="error-msg">
 
         </div>
         <form action="" class="bg-gray-300 h-[88vh]  rounded-tl-3xl rounded-br-3xl" id="form">
             <div class="flex justify-between items-start">
-                <a href="{{ url('/') }}" role="button" class="rounded-tl-3xl rounded-br-3xl px-5 py-4 bg-red-500 text-white">
+                <a href="{{ url('/') }}" role="button"
+                    class="rounded-tl-3xl rounded-br-3xl px-5 py-4 bg-red-500 text-white">
                     Back
                 </a>
                 <div>
+                    <button id="btn-show-cga" type="button" class="disabled:bg-gray-500 px-5 py-4 bg-indigo-700 text-white">
+                        CGA
+                    </button>
                     <button id="btn-show-blowback" type="button"
                         class="disabled:bg-gray-500 px-5 py-4 bg-indigo-700 text-white">
                         Blow Back
@@ -28,24 +32,35 @@
                                 Close
                             </button>
                         </div>
+                        <div id="cga-form" class="hidden flex-row space-x-3 items-center">
+                            <div class="text-red-500"></div>
+                            <button type="button" id="btn-start-cga"
+                                class="disabled:bg-gray-500 px-5 py-4 bg-indigo-700 text-white">
+                                Start CGA
+                            </button>
+                            <button type="button" id="btn-cancel-cga"
+                                class="disabled:bg-gray-500 px-5 py-4 bg-red-500 text-white">
+                                Close
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
             @foreach ($sensors as $sensor)
-            <div class="flex justify-between space-x-3 items-center" id="section-form">
-                <div class="w-1/2 px-6 py-3 border-r-2 border-gray-400">
-                    <button data-sensorId="{{ $sensor->id }}" type="button"
-                        class="btn-start btn_zero disabled:bg-gray-500 w-full py-4 h-[7rem] text-xl font-bold bg-indigo-500 text-white">
-                        {!! $sensor->name !!} ZERO Calibration
-                    </button>
+                <div class="flex justify-between space-x-3 items-center" id="section-form">
+                    <div class="w-1/2 px-6 py-3 border-r-2 border-gray-400">
+                        <button data-sensorId="{{ $sensor->id }}" type="button"
+                            class="btn-start btn_zero disabled:bg-gray-500 w-full py-4 h-[7rem] text-xl font-bold bg-indigo-500 text-white">
+                            {!! $sensor->name !!} ZERO Calibration
+                        </button>
+                    </div>
+                    <div class="w-1/2 px-6 py-3">
+                        <button data-sensorId="{{ $sensor->id }}" data-type="span" type="button"
+                            class="btn-start btn_span disabled:bg-gray-500 w-full py-4 h-[7rem] text-xl font-bold bg-indigo-500 text-white">
+                            {!! $sensor->name !!} SPAN Calibration
+                        </button>
+                    </div>
                 </div>
-                <div class="w-1/2 px-6 py-3">
-                    <button data-sensorId="{{ $sensor->id }}" data-type="span" type="button"
-                        class="btn-start btn_span disabled:bg-gray-500 w-full py-4 h-[7rem] text-xl font-bold bg-indigo-500 text-white">
-                        {!! $sensor->name !!} SPAN Calibration
-                    </button>
-                </div>
-            </div>
             @endforeach
         </form>
     </div>
@@ -59,7 +74,9 @@
                     url: `{{ url('api/calibration-start') }}/${sensorId}`,
                     type: 'POST',
                     dataType: 'json',
-                    data : {calibration_type : 1},
+                    data: {
+                        calibration_type: 1
+                    },
                     success: function(data) {
                         if (data.success) {
                             window.location.href =
@@ -74,7 +91,9 @@
                     url: `{{ url('api/calibration-start') }}/${sensorId}`,
                     type: 'POST',
                     dataType: 'json',
-                    data : {calibration_type : 2},
+                    data: {
+                        calibration_type: 2
+                    },
                     success: function(data) {
                         if (data.success) {
                             window.location.href =
@@ -89,12 +108,42 @@
         // Blowback function
         $(document).ready(function() {
             // Manipulate element HTML
+            $('#btn-show-cga').click(function() {
+                $('#btn-show-blowback').addClass('hidden')
+                $(this).addClass('hidden')
+                $('#cga-form').removeClass('hidden').addClass('flex')
+            })
+            $('#btn-cancel-cga').click(function() {
+                $('#btn-show-blowback').removeClass('hidden')
+                $('#btn-show-cga').removeClass('hidden')
+                $('#cga-form').addClass('hidden').removeClass('flex')
+            })
+            //
+            $('#btn-start-cga').click(function() {
+                $(this).html(`CGA...`)
+                $.ajax({
+                    url: `{{ url('api/blowback') }}`,
+                    type: 'PATCH',
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data.success) {
+                            setTimeout(() => {
+                                window.location.href =
+                                    `{{ url('cga/process') }}`
+                            }, 5000);
+                        }
+                    }
+                })
+            })
+
             $('#btn-show-blowback').click(function() {
                 $(this).addClass('hidden')
+                $('#btn-show-cga').addClass('hidden')
                 $('#blowback-form').removeClass('hidden').addClass('flex')
             })
             $('#btn-cancel-blowback').click(function() {
                 $('#btn-show-blowback').removeClass('hidden')
+                $('#btn-show-cga').removeClass('hidden')
                 $('#blowback-form').addClass('hidden').removeClass('flex')
             })
             //
@@ -108,7 +157,6 @@
                     dataType: 'json',
                     success: function(data) {
                         if (data.success) {
-                            // $('#btn-start-blowback').addClass('hidden')
                             $('#btn-cancel-blowback').addClass('hidden')
                             intervalBlowback = setInterval(checkBlowback, 1000);
                         }
