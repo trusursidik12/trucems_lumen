03d9b0b70f5e83e5159b477a2fd322119a783a4c
dulurprojects2019 <trusursidik12@gmail.com>
2022-08-09 22:06:50 +0700

    hide nox on calibration mode

--- resources/views/calibration/manual.blade.php
+++ resources/views/calibration/manual.blade.php
@@ -51,20 +51,22 @@
             <div class="h-[20rem] hidden"></div>
             <div class="h-[7rem] hidden"></div>
             @foreach ($sensors as $sensor)
-                <div class="flex justify-between space-x-3 items-center" id="section-form">
-                    <div class="w-1/2 px-6 py-3 border-r-2 border-gray-400">
-                        <button data-sensorId="{{ $sensor->id }}" type="button"
-                            class="btn-start btn_zero disabled:bg-gray-500 w-full py-4 h-[{{ $count == 1 ? 20 : ($count == 2 ? 12 : 7) }}rem] text-xl font-bold bg-indigo-500 text-white">
-                            {!! $sensor->name !!} ZERO Calibration
-                        </button>
+                @if ($sensor->id != 4)
+                    <div class="flex justify-between space-x-3 items-center" id="section-form">
+                        <div class="w-1/2 px-6 py-3 border-r-2 border-gray-400">
+                            <button data-sensorId="{{ $sensor->id }}" type="button"
+                                class="btn-start btn_zero disabled:bg-gray-500 w-full py-4 h-[{{ $count == 1 ? 20 : ($count == 2 ? 12 : 7) }}rem] text-xl font-bold bg-indigo-500 text-white">
+                                {!! $sensor->name !!} ZERO Calibration
+                            </button>
+                        </div>
+                        <div class="w-1/2 px-6 py-3">
+                            <button data-sensorId="{{ $sensor->id }}" data-type="span" type="button"
+                                class="btn-start btn_span disabled:bg-gray-500 w-full py-4 h-[{{ $count == 1 ? 20 : ($count == 2 ? 12 : 7) }}rem] text-xl font-bold bg-indigo-500 text-white">
+                                {!! $sensor->name !!} SPAN Calibration
+                            </button>
+                        </div>
                     </div>
-                    <div class="w-1/2 px-6 py-3">
-                        <button data-sensorId="{{ $sensor->id }}" data-type="span" type="button"
-                            class="btn-start btn_span disabled:bg-gray-500 w-full py-4 h-[{{ $count == 1 ? 20 : ($count == 2 ? 12 : 7) }}rem] text-xl font-bold bg-indigo-500 text-white">
-                            {!! $sensor->name !!} SPAN Calibration
-                        </button>
-                    </div>
-                </div>
+                @endif
             @endforeach
         </form>
     </div>
